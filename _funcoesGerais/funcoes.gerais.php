6db46872655066e6c94e9e1f0caeb71349d05258
alat <alat@grh1206>
2018-09-05 11:12:11 -0300

    varias alteracoes

--- _funcoesGerais/funcoes.gerais.php
+++ _funcoesGerais/funcoes.gerais.php
@@ -1354,7 +1354,219 @@ function dataExtenso($data = NULL){
         alert('Data Inválida');
         return FALSE;
     }  
-}   
+}
+
+###########################################################	
+
+function dataExtenso2($data = NULL){
+/**  
+ * Exibe a data por extenso todo em texto
+ * 
+ * @param $data	date NULL A data a ser transformada
+ *
+ * @return string A data por extenso totalmente em palavras
+ * 
+ * @syntax dataExtenso2([$data]);
+ * 
+ * @note Observe que o formato de entrada da data é o brasileiro e o divisor é o /.
+ * @note Se a data estiver no formato americano ou o divisor for diferente, a data deverá ser convertida.
+ * @note Se a $data não for informada, a função pegará a data atual. 
+ * 
+ * @example exemplo.dataExtenso.php  
+ */ 
+    
+    # Se for nula coloca a data atual
+    if(is_null($data)){
+        $data = date("d/m/Y");
+    }
+
+    # Verifica a validade da data
+    if(validaData($data)){
+        
+        # Divide a data em dia, mes e ano
+        $dt = explode('/',$data);
+        
+        # Transforma em numero
+        $dt[0] = intval($dt[0]);
+        $dt[1] = intval($dt[1]);
+        $dt[2] = intval($dt[2]);
+        
+        # Inicia variaveis
+        $inicio = NULL;
+        $dataExtenso = NULL;
+        
+        # pega o mês
+        $mes = strtolower(get_nomeMes($dt[1]));
+        
+        # Inicio do texto
+        switch ($dt[0]){
+            case 1:
+                $inicio = "Ao primeiro dia";
+                break;
+            case 2:
+                $inicio = "Ao segundo dia";
+                break;
+            case 3:
+                $inicio = "Ao terceiro dia";
+                break;
+            case 4:
+                $inicio = "Ao quarto dia";
+                break;
+            case 5:
+                $inicio = "Ao quinto dia";
+                break;
+            default:
+                $inicio = "Aos ".numero_to_letra($dt[0])." dias";
+                break;
+        }
+        
+        $dataExtenso =  $inicio.' de '.$mes.' de '.numero_to_letra(intval($dt[2]));
+      
+        return $dataExtenso;
+    }else{
+        alert('Data Inválida');
+        return FALSE;
+    }  
+}
+
+###########################################################
+
+function numero_to_letra($number) {
+
+/**  
+ * Converte um numero para palavra (exenso)
+ * 
+ * @param $number integer NULL O numero a ser tratado
+ *
+ * @return string A palavra
+ * 
+ * @syntax numero_to_letra($number);
+ */     
+
+    $hyphen      = '-';
+    $conjunction = ' e ';
+    $separator   = ', ';
+    $negative    = 'menos ';
+    $decimal     = ' ponto ';
+    $dictionary  = array(
+        0                   => 'zero',
+        1                   => 'um',
+        2                   => 'dois',
+        3                   => 'três',
+        4                   => 'quatro',
+        5                   => 'cinco',
+        6                   => 'seis',
+        7                   => 'sete',
+        8                   => 'oito',
+        9                   => 'nove',
+        10                  => 'dez',
+        11                  => 'onze',
+        12                  => 'doze',
+        13                  => 'treze',
+        14                  => 'quatorze',
+        15                  => 'quinze',
+        16                  => 'dezesseis',
+        17                  => 'dezessete',
+        18                  => 'dezoito',
+        19                  => 'dezenove',
+        20                  => 'vinte',
+        30                  => 'trinta',
+        40                  => 'quarenta',
+        50                  => 'cinquenta',
+        60                  => 'sessenta',
+        70                  => 'setenta',
+        80                  => 'oitenta',
+        90                  => 'noventa',
+        100                 => 'cento',
+        200                 => 'duzentos',
+        300                 => 'trezentos',
+        400                 => 'quatrocentos',
+        500                 => 'quinhentos',
+        600                 => 'seiscentos',
+        700                 => 'setecentos',
+        800                 => 'oitocentos',
+        900                 => 'novecentos',
+        1000                => 'mil',
+        1000000             => array('milhão', 'milhões'),
+        1000000000          => array('bilhão', 'bilhões'),
+        1000000000000       => array('trilhão', 'trilhões'),
+        1000000000000000    => array('quatrilhão', 'quatrilhões'),
+        1000000000000000000 => array('quinquilhão', 'quinquilhões')
+    );
+
+    if (!is_numeric($number)) {
+        return false;
+    }
+
+    if (($number >= 0 && (int) $number < 0) || (int) $number < 0 - PHP_INT_MAX) {
+        // overflow
+        trigger_error(
+            'convert_number_to_words só aceita números entre ' . PHP_INT_MAX . ' à ' . PHP_INT_MAX,
+            E_USER_WARNING
+        );
+        return false;
+    }
+
+    if ($number < 0) {
+        return $negative . numero_to_letra(abs($number));
+    }
+
+    $string = $fraction = null;
+
+    if (strpos($number, '.') !== false) {
+        list($number, $fraction) = explode('.', $number);
+    }
+
+    switch (true) {
+        case $number < 21:
+            $string = $dictionary[$number];
+            break;
+        case $number < 100:
+            $tens   = ((int) ($number / 10)) * 10;
+            $units  = $number % 10;
+            $string = $dictionary[$tens];
+            if ($units) {
+                $string .= $conjunction . $dictionary[$units];
+            }
+            break;
+        case $number < 1000:
+            $hundreds  = floor($number / 100)*100;
+            $remainder = $number % 100;
+            $string = $dictionary[$hundreds];
+            if ($remainder) {
+                $string .= $conjunction . numero_to_letra($remainder);
+            }
+            break;
+        default:
+            $baseUnit = pow(1000, floor(log($number, 1000)));
+            $numBaseUnits = (int) ($number / $baseUnit);
+            $remainder = $number % $baseUnit;
+            if ($baseUnit == 1000) {
+                $string = numero_to_letra($numBaseUnits) . ' ' . $dictionary[1000];
+            } elseif ($numBaseUnits == 1) {
+                $string = numero_to_letra($numBaseUnits) . ' ' . $dictionary[$baseUnit][0];
+            } else {
+                $string = numero_to_letra($numBaseUnits) . ' ' . $dictionary[$baseUnit][1];
+            }
+            if ($remainder) {
+                $string .= $remainder < 100 ? $conjunction : $separator;
+                $string .= numero_to_letra($remainder);
+            }
+            break;
+    }
+
+    if (null !== $fraction && is_numeric($fraction)) {
+        $string .= $decimal;
+        $words = array();
+        foreach (str_split((string) $fraction) as $number) {
+            $words[] = $dictionary[$number];
+        }
+        $string .= implode(' ', $words);
+    }
+
+    return $string;
+}
+
 
 ###########################################################
 
