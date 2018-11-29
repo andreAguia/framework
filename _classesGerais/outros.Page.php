d769a12745dec6f3b8d6b5ec3a05303e57675517
alat <alat@alat-pc>
2018-11-28 22:36:57 -0200

    Envio de email

--- _classesGerais/outros.Page.php
+++ _classesGerais/outros.Page.php
@@ -240,9 +240,9 @@ class Page
      */
         # Java Script do Foundation
         #echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'what-input.js"></script>';
-        #echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'foundation.js"></script>';
-        echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'foundation.min.js"></script>'; 
-        echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'app.js"></script>';
+        echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'foundation.js"></script>';
+        #echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'foundation.min.js"></script>'; 
+        #echo '<script language="JavaScript" src="'.PASTA_FUNCOES_GERAIS.'app.js"></script>';
 
         echo '</body>';
         echo '<footer id="center">';        
