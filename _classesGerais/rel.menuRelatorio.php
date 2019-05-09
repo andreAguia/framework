cc90649994ec8c30bf8c70b65d39c629abcee645
andre <andre@grh1206>
2019-05-09 08:43:41 -0300

    Ato do Reitor

--- _classesGerais/rel.menuRelatorio.php
+++ _classesGerais/rel.menuRelatorio.php
@@ -163,6 +163,7 @@ class menuRelatorio
             
             # Exibe o aviso (quando tiver)
             if(!is_null($this->aviso)){
+                br();
                 callout($this->aviso);
             }
 
