e1d35f0f225df32897cea9c64fd5a3ee762cfa83
André Águia <alataguia@gmail.com>
2025-08-26 15:03:59 -0300

    função formatacpf

--- _funcoesGerais/funcoes.gerais.php
+++ _funcoesGerais/funcoes.gerais.php
@@ -3140,3 +3140,19 @@ function ressaltaSimNao($texto) {
 
 ###########################################################
 
+function formatCnpjCpf($value) {
+
+    # O tamanho normal de um cpf
+    $CPF_LENGTH = 11;
+    
+    # Preenche com Zeros
+    $value = str_pad($value, $CPF_LENGTH, "0", STR_PAD_LEFT);
+
+    $cnpj_cpf = preg_replace("/\D/", '', $value);
+
+    if (strlen($cnpj_cpf) === $CPF_LENGTH) {
+        return preg_replace("/(\d{3})(\d{3})(\d{3})(\d{2})/", "\$1.\$2.\$3-\$4", $cnpj_cpf);
+    }
+
+    return preg_replace("/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/", "\$1.\$2.\$3/\$4-\$5", $cnpj_cpf);
+}
